7efc81e04c5e002431e6cd0cb57f6fb66487b322
Callum Quirk <callumbquirk@gmail.com>
2023-02-04 19:34:27 +0000

    comitting function that finds the current weather information for specified city, logging current date, the icon, temperature, humidity, and wind speed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,11 @@
 let cityLat = ""
 let cityLon = ""
+let apiKey ="81181bda96c8c7fd7c78e47ac25b3020"
+let currentDate = ""
+let currentIcon = ""
+let currentTemperature = ""
+let currentHumdity = ""
+let currentWind =""
 
 $('#search-button').on("click", function(){
     event.preventDefault();
@@ -9,14 +15,32 @@ $('#search-button').on("click", function(){
 function getCity(){
     let cityName = $("#search-input").val()
     console.log(cityName)
-    let apiKey ="81181bda96c8c7fd7c78e47ac25b3020"
     let queryURL = "http://api.openweathermap.org/geo/1.0/direct?q="+cityName+"&limit=10&appid="+apiKey
     $.ajax({
         url:queryURL,
         method:"GET"
     }).then(function(response) {
-        cityLat = response[0].lat
-        cityLon = response[0].lon
+        cityLat= response[0].lat;
+        cityLon= response[0].lon;
+        currentWeather();
+    }
+    )
+}
+
+function currentWeather(){
+    let queryURL = "https://api.openweathermap.org/data/2.5/weather?lat="+cityLat+"&lon="+cityLon+"&appid="+apiKey
+    console.log(cityLat)
+    console.log(cityLon)
+    $.ajax({
+        url:queryURL,
+        method:"GET"
+    }).then(function(response) {
+        console.log(response)
+        currentDate = moment()
+        currentIcon = response.weather[0].icon
+        currentTemperature = response.main.temp
+        currentHumdity = response.main.humidity
+        currentWind = response.wind.speed
     }
     )
 }

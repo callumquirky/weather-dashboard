df1698002a908b01d6434ebc965119171d930fb5
Callum Quirk <callumbquirk@gmail.com>
2023-02-04 20:15:39 +0000

    added function to gather data for 5 day forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,27 @@ let currentIcon = ""
 let currentTemperature = ""
 let currentHumdity = ""
 let currentWind =""
+let day1Date = ""
+let day1Icon = ""
+let day1Temperature = ""
+let day1Hudmity = ""
+let day2Date = ""
+let day2Icon = ""
+let day2Temperature = ""
+let day2Hudmity = ""
+let day3Date = ""
+let day3Icon = ""
+let day3Temperature = ""
+let day3Hudmity = ""
+let day4Date = ""
+let day4Icon = ""
+let day4Temperature = ""
+let day4Hudmity = ""
+let day5Date = ""
+let day5Icon = ""
+let day5Temperature = ""
+let day5Hudmity = ""
+
 
 $('#search-button').on("click", function(){
     event.preventDefault();
@@ -20,22 +41,20 @@ function getCity(){
         url:queryURL,
         method:"GET"
     }).then(function(response) {
-        cityLat= response[0].lat;
-        cityLon= response[0].lon;
-        currentWeather();
+        cityLat = response[0].lat;
+        cityLon = response[0].lon;
+        currentForecast();
+        fiveDayForecast();
     }
     )
 }
 
-function currentWeather(){
+function currentForecast(){
     let queryURL = "https://api.openweathermap.org/data/2.5/weather?lat="+cityLat+"&lon="+cityLon+"&appid="+apiKey
-    console.log(cityLat)
-    console.log(cityLon)
     $.ajax({
         url:queryURL,
         method:"GET"
     }).then(function(response) {
-        console.log(response)
         currentDate = moment()
         currentIcon = response.weather[0].icon
         currentTemperature = response.main.temp
@@ -44,3 +63,35 @@ function currentWeather(){
     }
     )
 }
+
+function fiveDayForecast(){
+    let queryURL = "https://api.openweathermap.org/data/2.5/forecast?lat="+cityLat+"&lon="+cityLon+"&appid="+apiKey
+    $.ajax({
+        url:queryURL,
+        method:"GET"
+    }).then(function(response) {
+        console.log(response)
+        day1Date = response.list[1].moment(dt).format("D/M/YYYY")
+        day1Icon = response.list[1].weather.icon
+        day1Temperature = response.list[1].main.temp
+        day1Hudmity = response.list[1].main.humidity
+        day2Date = response.list[2].moment(dt).format("D/M/YYYY")
+        day2Icon = response.list[2].weather.icon
+        day2Temperature = response.list[2].main.temp
+        day2Hudmity = response.list[2].main.humidity
+        day3Date = response.list[3].moment(dt).format("D/M/YYYY")
+        day3Icon = response.list[3].weather.icon
+        day3Temperature = response.list[3].main.temp
+        day3Hudmity = response.list[3].main.humidity
+        day4Date = response.list[4].moment(dt).format("D/M/YYYY")
+        day4Icon = response.list[4].weather.icon
+        day4Temperature = response.list[4].main.temp
+        day4Hudmity = response.list[4].main.humidity
+        day5Date = response.list[5].moment(dt).format("D/M/YYYY")
+        day5Icon = response.list[5].weather.icon
+        day5Temperature = response.list[5].main.temp
+        day5Hudmity = response.list[5].main.humidity
+    }
+    )
+}
+
